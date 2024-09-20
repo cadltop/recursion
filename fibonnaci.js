40b9042fd5940268799c654b8efda0ec515fe5d4
cadltop <caesarfromthecross.808@gmail.com>
2024-09-20 16:09:42 -0500

    Solve fibonacci with recursion

--- fibonnaci.js
+++ fibonnaci.js
@@ -3,6 +3,7 @@ function fibs(number) {
     let i = 0;
     let sequence = [];
     let value;
+    
     while(i < number) {
         if(i >= 2){
             value = sequence[i-1] + sequence[i-2];
@@ -14,5 +15,21 @@ function fibs(number) {
     }
     return sequence;
 }
-
 // console.log(fibs(8));
+
+// Recursion
+function fibsRec(number) {
+    if (number < 2){
+        return number;
+    }
+    return fibsRec(number-1) + fibsRec(number-2);
+}
+
+function showRecursion(n) {
+    let sequence = []
+    for (let i = 0; i < n; i++) {
+        sequence.push(fibsRec(i));
+    }
+    return sequence;
+}
+//console.log(showRecursion(8));
